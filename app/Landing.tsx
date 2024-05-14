3125a6be73c344882bb25b65187af23cf6706332
christianhubbard <chrishubbard92@hotmail.com>
2024-05-13 17:46:16 -0700

    Updating landing

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { User } from './user';
 import { Button } from '../components/ui/button';
@@ -25,6 +26,90 @@ const Landing = () => {
         >
           <Button variant="secondary">Learn More</Button>
         </Link>
+      </div>
+      <div className="py-32">
+        <div className="text-3xl sm:text-4xl md:text-6xl py-6 md:py-8">
+          Features
+        </div>
+        <div className="flex flex-col md:flex-row py-4 md:py-12 items-center justify-around  w-full xl:w-10/12 mx-auto">
+          <div className="hidden md:block w-96 h-72 overflow-hidden drop-shadow py-4 sm:py-8">
+            <Image
+              alt="NextAuth Logo"
+              src="/images/next-auth.jpg"
+              className="object-contain "
+              layout="fill"
+            />
+          </div>
+          <div className="w-10/12 sm:w-9/12 md:w-5/12 py-4 sm:py-6">
+            <div className="py-4 text-xl sm:text-2xl md:text-4xl">
+              OAuth Registration
+            </div>
+            <div className="py-4 sm:py-6">
+              When a user signs up with our Github OAuth integration, we
+              automatically create an entry for them in our Database. This
+              ensures that our application data stays in sync with our
+              authentication records, and allows users to begin tracking their
+              pomodoros immediately after signing up.
+            </div>
+          </div>
+          <div className="block md:hidden w-96 h-72 overflow-hidden drop-shadow py-4 sm:py-8">
+            <Image
+              alt="NextAuth Logo"
+              src="/images/next-auth.jpg"
+              className="object-contain "
+              layout="fill"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row py-4 md:py-12 items-center justify-around w-full xl:w-10/12 mx-auto">
+          <div className="w-10/12 sm:w-9/12 md:w-5/12 py-4 sm:py-6">
+            <div className="py-8 text-xl sm:text-2xl md:text-4xl">
+              Persistent Timer
+            </div>
+            <div className="py-4 sm:py-6">
+              We leverage Vercel KV to keep a temporary store of your current
+              timer on the edge. Closing the browser, reloading the page, or
+              navigating to the leaderboard won't reset your progress on your
+              current timer.
+            </div>
+          </div>
+          <div className="relative w-96 h-72 overflow-hidden drop-shadow py-4 sm:py-8">
+            <Image
+              alt="timer in progress"
+              src="/images/timers.png"
+              className="object-cover "
+              layout="fill"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row py-4 md:py-12 items-center justify-around  w-full xl:w-10/12 mx-auto">
+          <div className="hidden md:block w-96 md:w-1/2 lg:w-1/2 h-72 overflow-hidden drop-shadow py-4 sm:py-8">
+            <Image
+              alt="table of users sorted by pomodoros completed"
+              src="/images/leaderboard.png"
+              className="object-contain "
+              layout="fill"
+            />
+          </div>
+          <div className="w-10/12 sm:w-9/12 md:w-5/12 py-4 sm:py-6">
+            <div className="py-4 text-xl sm:text-2xl md:text-4xl">
+              Pomodoro Leaderboards
+            </div>
+            <div className="py-4 sm:py-6">
+              Vercel's Postgres database allows us to keep an intricate log of
+              all pomodoros completed. The leaderboard ranks the users by the
+              number of pomodoros they've completed and is updated in real-time.
+            </div>
+          </div>
+          <div className="block md:hidden w-10/12 h-72 overflow-hidden drop-shadow py-4 sm:py-8">
+            <Image
+              alt="table of users sorted by pomodoros completed"
+              src="/images/leaderboard.png"
+              className="object-contain "
+              layout="fill"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
